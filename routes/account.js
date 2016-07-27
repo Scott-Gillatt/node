2be6a5136719d98de8aba8993a04845b9a06b787
Scott Gillatt <scottgillatt@gmail.com>
2016-07-26 18:40:58 -0600

    Got Tom Changes

--- routes/account.js
+++ routes/account.js
@@ -3,47 +3,50 @@ var router = require('express').Router();
 var user = null;
 
 router.route('/login')
-    .post(function (req, res, next) {
+	.post(function (req, res, next) {
 
-        user = {
-            username: req.body.username,
-            password: req.body.password
-        };
+		user = {
+			username: req.body.username,
+			password: req.body.password
+		};
 
-        if (user.username && user.password) {
-            res.send({
-                success: true,
-                status: 'Logged in.'
-            });
-        } else {
-            throw new Error('Username or Password missing')
-            // res.send({
-            //     success:false,
-            //     status: 'Username and password required'
-            // })
-        } 
-    });
+		if (user.username && user.password) {
+			res.send({
+				success: true,
+				status: 'Logged in.'
+			});
+		}
+		else {
+			throw new Error('Username or password missing!');
+		}
+
+	});
 
 router.route('/session')
-.get(function (req,res,next){
-    res.send(user);
-})
+	.get(function (req, res, next) {
+
+		res.send(user);
+	});
 
 router.route('/logout')
-    .get(function (req, res, next) {
-        user = null;
-        res.send({
-            success: true,
-            status: 'Logged Out!'
-        })
-    });
-router.use(function(err, req, res, next){
-   console.log("Error: ", err);
-   return next(err);
+	.get(function (req, res, next) {
+
+		user = null;
+		
+		res.send({
+			success: true,
+			status: 'Logged out.'
+		});
+	});
+
+router.use(function (err, req, res, next) {
+    console.log('Error: ', err);
+	return next(next);
 });
 
-router.use(function(err, req, res, next){
-    res.json({success: false, error: { name:err.name, message: err.message } });
+router.use(function (err, req, res, next) {
+    res.json({ success: false, error: { name: err.name, message: err.message } });
 });
+
 
 exports.router = router;
--- routes/todos.js
+++ routes/todos.js
@@ -1,79 +1,92 @@
 var router = require('express').Router();
+var db = new loki('todos.json');
+
+var todos = db.addCollectoin('children');
 
 var todos = [];
 
 router.route('/add')
-    .post(function (req, res, next) {
+	.post(function (req, res, next) {
 
-        todo = {
-            task: req.body.task,
-        };
+		if (req.body.task) {
+			todos.push(req.body.task);
+			res.send(todos);
+		}
+		else {
+			throw new Error('A task is required!');
+		}
 
-        if (req.body.task) {
-            todos.push(req.body.task)
-            res.send(todos);
-        } else {
-            throw new Error('A task is required');
-        }
-
-    });
+	});
 
 router.route('/:index')
-    .put(function (req, res, next) {
-        var index = req.params.index;
+	.put(function (req, res, next) {
 
-        if (index < todos.length || index < 0) {
-            if (req.body.task) {
-                todos[index] = req.body.task;
+		var index = req.params.index;
 
-                res.send(todos);
-            }
-        } else {
-            throw new Error('A task is required!')
-        }
-    })
+		if (index < todos.length || index < 0) {
+			if (req.body.task) {
+
+				todos[index] = req.body.task;
+
+				res.send(todos);
+			}
+			else {
+				throw new Error('A task is required!');
+			}
+
+		}
+		else {
+			throw new Error('That index does not exist!');
+		}
+	});
 
 router.route('/:index?')
-    .get(function (req, res, next) {
+	.get(function (req, res, next) {
 
-        if (req.params.index) {
-            var index = req.params.index;
+		if (req.params.index) {
+			var index = req.params.index;
 
-            if (index < todos.length || index < 0) {
-                res.send(todos[index]);
-            } else {
-                throw new Error('That index does not exist!')
-            }
-        } else {
-            res.send(todos);
-        }
+			if (index < todos.length || index < 0) {
+				res.send(todos[index]);
+			}
+			else {
+				throw new Error('That index does not exist!');
+			}
+		}
+		else {
+			res.send(todos);
+		}
 
-    })
-    .delete(function (req, res, netxt) {
-        if (req.params.index) {
-            var index = req.params.index;
+	})
+	.delete(function (req, res, next) {
 
-            if (index < todos.length || index < 0) {
-                todos.splice(index, 1);
+		if (req.params.index) {
+			var index = req.params.index;
 
-                res.send(todos);
-            }
-            else {
-                throw new Error('That index does not exist!');
-            }
-        }
-        else {
-            throw new Error('An index is required!');
-        }
-    });
+			if (index < todos.length || index < 0) {
 
-router.use(function (err, req, res, next) {
-    console.log("Error: ", err);
-    return next(err);
-});
+				todos.splice(index, 1);
+
+				res.send(todos);
+			}
+			else {
+				throw new Error('That index does not exist!');
+			}
+		}
+		else {
+			throw new Error('An index is required!');
+		}
+
+	});
+
+// router.use(function (err, req, res, next) {
+//     console.log('Error: ', err);
+// 	return next(next);
+// });
 
 router.use(function (err, req, res, next) {
     res.json({ success: false, error: { name: err.name, message: err.message } });
 });
+
 
 exports.router = router;
